889e6662911d4ed5980a61b084d8f60c3ff3fcaa
marthyndl <marthyndl@gmail.com>
2019-06-04 18:23:06 -0300

    Carro de Compras de Robots

---
+++ src/shared/CollapsibleSection.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <div class="header">
+            <span v-if="open" @click="open = !open" >&#x25B2; Cerrar</span>
+            <span v-if="!open" @click="open = !open">&#x25BC; Expandir</span>
+        </div>
+        <!-- Toggleable -->
+        <slot v-if="open">
+            <div>defaultContent</div>
+        </slot>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CollapsibleSection',
+    data() {
+        return {
+            open: true,
+        }
+    }
+}    
+</script>
+
+<style scoped>
+.header {
+    background-color: #bbb;
+    padding: 3px;
+    cursor: pointer;
+}
+</style>
